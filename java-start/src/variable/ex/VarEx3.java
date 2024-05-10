272e151e352d533e253002e3acb253f14b3c3bf9
jinkwonpark <jk3473@naver.com>
2024-05-10 23:14:12 +0900

    Section 02. 변수

---
+++ java-start/src/variable/ex/VarEx3.java
@@ -0,0 +1,15 @@
+package variable.ex;
+
+import java.util.Arrays;
+
+public class VarEx3 {
+
+    public static void main(String[] args) {
+        long longVar = 10000000000L;
+        System.out.println(longVar);  // sout
+        System.out.println("longVar = " + longVar);  // soutv
+
+        boolean booleanVar = true;
+        System.out.println(booleanVar);
+    }
+}
