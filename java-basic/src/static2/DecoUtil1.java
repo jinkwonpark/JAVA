693c7def6a6205fd7ef90bc2eff2e6cbf796ba04
jinkwonpark <jk3473@naver.com>
2024-06-28 17:13:13 +0900

    Section 07. 자바 메모리 구조와 static

---
+++ java-basic/src/static2/DecoUtil1.java
@@ -0,0 +1,10 @@
+package static2;
+
+public class DecoUtil1 {
+
+    public String deco(String str) {
+        String result = "*" + str + "*";
+        return result;
+        // return "*" + str + "*";  // 위의 두 줄을 이렇게 생략 가능
+    }
+}
