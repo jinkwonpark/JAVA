b2dbaaf61b5a71299620aaf2d717c06a7f04a354
jinkwonpark <jk3473@naver.com>
2024-07-18 13:55:26 +0900

    Section 01. Object 클래스

---
+++ java-mid1/src/lang/object/equals/UserV2.java
@@ -0,0 +1,30 @@
+package lang.object.equals;
+
+import java.util.Objects;
+
+public class UserV2 {
+
+    private String id;
+
+    public UserV2(String id) {
+        this.id = id;
+    }
+
+/*
+    @Override
+    public boolean equals(Object obj) {
+        UserV2 user = (UserV2) obj;
+        return id.equals(user.id);
+    }
+*/
+
+    // 변경 - 정확한 equals 구현, IDE 자동 생성
+    // Generate(Alt+Insert) > equals() and hashCode()
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        UserV2 userV2 = (UserV2) object;
+        return Objects.equals(id, userV2.id);
+    }
+}
